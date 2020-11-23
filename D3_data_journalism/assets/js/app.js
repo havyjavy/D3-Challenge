813d1b662a417d852bab7512d02f345f801f1bd4
HavyJavy <portocarrero.javier@gmail.com>
2020-11-23 15:13:58 -0600

    load csv and built basic framework

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,69 @@
 // @TODO: YOUR CODE HERE!
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+
+
+// load data from csv
+d3.csv("/assets/data/data.csv").then(function (popData) {
+    console.log(popData);
+
+    // Cast each rate value in popData as a number using the unary + operator
+    popData.forEach(function (data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+        console.log("Poverty:", data.poverty);
+    });
+
+
+    // Create a scale for your independent (x) coordinates
+    var xScale = d3.scaleLinear()
+        .domain(d3.extent(popData, d => d.poverty))
+        .range([0, svgWidth]);
+
+    // Create a scale for your dependent (y) coordinates
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(popData, d => d.healthcare)])
+        .range([svgHeight, 0]);
+
+
+
+    // @TODO
+    // Create code to build the scatter plot using the popData.
+    chartGroup.selectAll("dot")
+        .data(popData)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d) { return x(d.poverty); })
+        .attr("cy", function (d) { return y(d.healthcare); })
+        .attr("r", 1.5)
+        .style("fill", "69b3a2")
+}).catch(function (error) {
+    console.log(error);
+})
+
